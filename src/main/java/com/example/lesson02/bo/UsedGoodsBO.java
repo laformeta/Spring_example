92d5fd7d0c63067cdc1e40c7fb5e9e1c537e0a56
kims <test@test.com>
2023-12-15 18:43:56 +0900

    2023년 12월 15일 수업완료

--- src/main/java/com/example/lesson02/bo/UsedGoodsBO.java
+++ src/main/java/com/example/lesson02/bo/UsedGoodsBO.java
@@ -2,15 +2,20 @@ package com.example.lesson02.bo;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.lesson02.domain.UsedGoods;
+import com.example.lesson02.mapper.UsedGoodsMapper;
 
 @Service
 public class UsedGoodsBO {
+	
+	@Autowired
+	private UsedGoodsMapper usedGoodsMapper;
 
 	// input : X(컨트롤러 에게서 받음)			output : (Repository한테 받은 것을) List<UsedGoods> (컨트롤러에게 줌)
 	public List<UsedGoods> getUsedGoodsList() {
-		return ;
+		return usedGoodsMapper.selectUsedGoodsList();
 	}
 }
