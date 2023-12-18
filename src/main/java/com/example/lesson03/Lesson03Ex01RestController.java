dab7bcf65552d4e00028e381a14e4918bfa3cf8e
kims <test@test.com>
2023-12-18 18:42:06 +0900

    12월 18일 수업끝

--- src/main/java/com/example/lesson03/Lesson03Ex01RestController.java
+++ src/main/java/com/example/lesson03/Lesson03Ex01RestController.java
@@ -2,6 +2,7 @@ package com.example.lesson03;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.lesson03.bo.ReviewBO;
@@ -13,9 +14,19 @@ public class Lesson03Ex01RestController {
 	@Autowired
 	private ReviewBO reviewBO;
 
-	// url : http://localhost/lesson03/ex01
+	// url : http://localhost/lesson03/ex01?id=3
 	@RequestMapping("/lesson03/ex01")
-	public Review ex01() {
-		return reviewBO.getReview(); // response body JSON
+	public Review ex01(
+			//@RequestParam(value = "id") int id // 필수 파라미터
+			//@RequestParam(value = "id", required = true) int id // 필수
+			//@RequestParam(value = "id", required = false) Integer id // 필수가 아닌 파라미터
+			//@RequestParam(value = "id", defaultValue = "1") int id // 필수 아니고 디폴트 값 1
+			@RequestParam("id") int id) { // 필수 
+		
+//		if (id == null) {
+//			id = 1;
+//		}
+		
+		return reviewBO.getReview(id); // response body JSON
 	}
 }
--- src/main/java/com/example/lesson03/bo/ReviewBO.java
+++ src/main/java/com/example/lesson03/bo/ReviewBO.java
@@ -12,8 +12,8 @@ public class ReviewBO {
 	@Autowired
 	private ReviewMapper reviewMapper;
 
-	// input : x  output : Review
-	public Review getReview() {
-		return reviewMapper.selectReview();
+	// input : id  output : Review
+	public Review getReview(int id) {
+		return reviewMapper.selectReview(id);
 }
 }
