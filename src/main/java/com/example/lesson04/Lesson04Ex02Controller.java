58582dba333ec9fee3d458c0a5c34c00dc7bbfa7
KIMSEONGHUN\lafor <laformeta@gmail.com>
2023-12-26 01:33:42 +0900

    lesson 05 ex02까지 완료

--- src/main/java/com/example/lesson04/Lesson04Ex02Controller.java
+++ src/main/java/com/example/lesson04/Lesson04Ex02Controller.java
@@ -2,6 +2,7 @@ package com.example.lesson04;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,7 +14,7 @@ import com.example.lesson04.domain.Student;
 @RequestMapping("/lesson04/ex02")
 @Controller
 public class Lesson04Ex02Controller {
-	
+
 	@Autowired
 	private StudentBO studentBO;
 
@@ -23,21 +24,22 @@ public class Lesson04Ex02Controller {
 	public String addStudentView() {
 		return "lesson04/addStudent";
 	}
-	
+
 	@PostMapping("/add-student")
-	public String addStudent(
-			@ModelAttribute Student student // 태그의 name 속성 파라미터와 이름이 같은 필드에 매핑됨 
-			) {
-		
+	public String addStudent(@ModelAttribute Student student, // 태그의 name 속성 파라미터와 이름이 같은 필드에 매핑됨
+	Model model) {
+
 		// DB insert
 		studentBO.addStudent(student);
-		
+
 		// DB에서 방금 가입된 사용자 select
 		int id = student.getId();
-		// select by id 완성해오기
-		
+		// selectStudentById => 완성
+		Student latestStudent = studentBO.getStudentById(id);
+
 		// Model 객체에 담는다.(jsp에서 사용하도록)
-		
+		model.addAttribute("student", latestStudent);
+
 		// 화면 뿌림(테이블로)
 		return "lesson04/afterAddStudent";
 	}
